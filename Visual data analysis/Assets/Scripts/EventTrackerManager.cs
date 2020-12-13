ecbc1d7deb9b437337439562a2f95400c08b8e84
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-12-13 11:09:20 +0100

    ReadData created + Resources folder

--- Visual data analysis/Assets/Scripts/EventTrackerManager.cs
+++ Visual data analysis/Assets/Scripts/EventTrackerManager.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class EventTrackerManager : SaveData
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+
+        List<Dictionary<string, object>> data = ReadData.Read("Saved_data");
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            print("Type " + data[i]["Type"] + " " +
+                   "TimeStamp " + data[i]["TimeStamp"] + " " +
+                   "PositionX " + data[i]["PositionX"] + " " +
+                   "PositionY " + data[i]["PositionY"]);
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.KeypadPlus))
         {
-            Debug.Log("hello");
+            Debug.Log("Saving Data into .CSV File...");
             SaveEvents();
             SavePosition();
         }
     }
 
+    
     private void OnApplicationQuit()
     {
-        //Writte xml/json/csv
-
         SavePosition();
         SaveEvents();
-
     }
 }
